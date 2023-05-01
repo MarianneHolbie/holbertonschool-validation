ba984f3298b600aa07fd4a31246ce57d5a718359
Arrue Marianne <5608@holbertonstudents.com>
2023-05-01 05:30:02 -0700

    remove command to get docker image

--- module1_task3/setup.sh
+++ module1_task3/setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# use docker image to dispose of Ubunto 18.04
-docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
+
 # same installation of GoHugo and Make
 apt-get update && apt-get install -y hugo make
 # test command
