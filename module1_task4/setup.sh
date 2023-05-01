70a9538ae24d0b15cc74ff03f94d1a15fb3fc17a
Arrue Marianne <5608@holbertonstudents.com>
2023-05-01 07:17:14 -0700

    add module1_4 directory

---
+++ module1_task4/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
+# same installation of GoHugo and Make
+apt-get update && apt-get install -y hugo make
+# download/install minimal version to use template ananke
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+apt install ./hugo.deb
+#remove file after installation
+rm hugo.deb
+# test command
+make build
