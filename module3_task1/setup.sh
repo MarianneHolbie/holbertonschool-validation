11f598fd9f011a07d0fb557d0c064ff98ef3199f
Arrue Marianne <5608@holbertonstudents.com>
2023-05-15 07:22:52 -0700

    remove command mv hugo

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -5,7 +5,6 @@ sudo apt-get update && sudo apt-get install -y hugo, make, git
 # download/install minimal version to use template ananke
 sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
 sudo apt install ./hugo.deb
-sudo mv ./hugo /usr/bin/
 #remove file after installation
 sudo rm hugo.deb
 
