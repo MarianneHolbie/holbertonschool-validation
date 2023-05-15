20a645cdfe089092bb063047e971646c74551916
Arrue Marianne <5608@holbertonstudents.com>
2023-05-15 06:41:41 -0700

    add sudo before command

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # download/install minimal version to use template ananke
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
-apt install ./hugo.deb
-mv ./hugo /usr/bin/
+sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+sudo apt install ./hugo.deb
+sudo mv ./hugo /usr/bin/
 #remove file after installation
-rm hugo.deb
+sudo rm hugo.deb
 
 # download/install linter
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@
-npm install -g markdownlint-cli
-npm install -g markdown-link-check
+sudo go install github.com/golangci/golangci-lint/cmd/golangci-lint@
+sudo npm install -g markdownlint-cli
+sudo npm install -g markdown-link-check
+
