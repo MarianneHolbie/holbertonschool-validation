4b0e7113c5a10b5f03b42dd3f2bd95e7fd5d3434
Arrue Marianne <5608@holbertonstudents.com>
2023-05-15 07:31:02 -0700

    solve problem

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get update && sudo apt-get install -y hugo, make, git
+sudo apt-get update && sudo apt-get install -y make git
 
 # download/install minimal version to use template ananke
 sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
@@ -9,7 +9,7 @@ sudo apt install ./hugo.deb
 sudo rm hugo.deb
 
 # download/install linter
-sudo go install github.com/golangci/golangci-lint/cmd/golangci-lint@
+sudo go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.2
 sudo npm install -g markdownlint-cli
 sudo npm install -g markdown-link-check
 
