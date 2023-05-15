bd321f8f4c5605efbbf967e44bb1fc09cdfe1792
Arrue Marianne <5608@holbertonstudents.com>
2023-05-15 04:38:27 -0700

    upload file for task 1

---
+++ module3_task1/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# download/install minimal version to use template ananke
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
+apt install ./hugo.deb
+#remove file after installation
+rm hugo.deb
+
