c64106f408d30ed93676a09ec6a3cfc5ff4b1bfc
Arrue Marianne <5608@holbertonstudents.com>
2023-05-17 01:59:06 -0700

    update file

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get update && sudo apt-get install -y make git zip
+
 # download/install minimal version to use template ananke
 sudo curl -L https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.84.0_Linux-64bit.deb -o hugo.deb
 sudo apt install ./hugo.deb
@@ -11,3 +13,6 @@ sudo rm hugo.deb
 sudo npm install -g markdownlint-cli
 sudo npm install -g markdown-link-check
 
+# Uninstall go
+sudo apt-get remove golang-go
+rm -rf /usr/local/go
